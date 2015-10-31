9db384a4988e53da1c23c56f7eb0eeeca462bce1
Daniel Padilla <danperzipa@hotmail.com>
2015-10-31 12:49:14 -0500

    .

--- app/src/main/java/com/ingdanielpadilla/listviewparse/MainActivity.java
+++ app/src/main/java/com/ingdanielpadilla/listviewparse/MainActivity.java
@@ -1,19 +1,16 @@
 package com.ingdanielpadilla.listviewparse;
 
-import android.app.DownloadManager;
 import android.app.ProgressDialog;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
@@ -26,14 +23,15 @@ public class MainActivity extends AppCompatActivity {
     private ListView mLv;
     private List<ParseObject> ob;
     ArrayList values= new ArrayList<String>();
+    CustomAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
         mLv=(ListView)findViewById(R.id.listView);
+        adapter=new CustomAdapter(MainActivity.this,values);
+        mLv.setAdapter(adapter);
 
 
 
@@ -81,8 +79,9 @@ public class MainActivity extends AppCompatActivity {
 
 
             try{
-                ParseQuery<ParseObject> query= new ParseQuery<ParseObject>("Data Entry");
+                ParseQuery<ParseObject> query= new ParseQuery<ParseObject>("DataEntry");
                 ob= query.find();
+                values.clear();
                 for(ParseObject dato : ob){
                     values.add(dato.get("first")+" "+dato.get("last"));
                 }
@@ -95,8 +94,9 @@ public class MainActivity extends AppCompatActivity {
         }
 
         protected void onPostExecute(Void result){
-            CustomAdapter adapter=new CustomAdapter(MainActivity.this,values);
-            mLv.setAdapter(adapter);
+
+            adapter.notifyDataSetChanged();
+
         }
     }
     @Override
